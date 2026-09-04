5d4eeb9e2772640abb99825e54ada2eebe0f347a
Ahmed_Mansour <133324agh@gmail.com>
2026-09-04 21:20:46 +0300

    Polish mobile navigation and examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,15 +100,15 @@ export default function Home() {
               <Link to="/docs/tutorial/getting-started" className="kyna-btn kyna-btn--primary">
                 Get started
               </Link>
-              <Link to="/docs/tutorial/tour" className="kyna-btn kyna-btn--secondary">
-                Language tour
-              </Link>
               <a
                 href="https://github.com/Up-to-code/Kyna"
                 target="_blank"
                 rel="noreferrer"
-                className="kyna-btn kyna-btn--ghost"
+                className="kyna-btn kyna-btn--secondary"
               >
+                <svg className="kyna-btn__icon" aria-hidden="true" viewBox="0 0 24 24">
+                  <path d="M12 2C6.48 2 2 6.58 2 12.23c0 4.52 2.87 8.35 6.84 9.71.5.09.68-.22.68-.49v-1.9c-2.78.62-3.37-1.37-3.37-1.37-.45-1.18-1.11-1.5-1.11-1.5-.91-.63.07-.62.07-.62 1 .07 1.53 1.05 1.53 1.05.89 1.56 2.34 1.11 2.91.85.09-.66.35-1.11.64-1.36-2.22-.26-4.56-1.14-4.56-5.05 0-1.12.39-2.03 1.03-2.74-.1-.26-.45-1.3.1-2.71 0 0 .84-.27 2.75 1.05A9.4 9.4 0 0 1 12 6.8c.85 0 1.71.12 2.5.34 1.91-1.32 2.75-1.05 2.75-1.05.55 1.41.2 2.45.1 2.71.64.71 1.03 1.62 1.03 2.74 0 3.93-2.34 4.79-4.57 5.04.36.32.68.94.68 1.9v2.97c0 .27.18.59.69.49A10.24 10.24 0 0 0 22 12.23C22 6.58 17.52 2 12 2Z" />
+                </svg>
                 GitHub
               </a>
             </div>
